ae164e2f551ba3e625ed2c935fc0ecba23fa82f9
frazao-hub <angelofrazao34@yahoo.com>
2026-09-06 05:29:46 -0300

    Implement service worker caching for files

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-sw.js
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('frazao-store').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});

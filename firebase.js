cc33491bc49060bc61da7970f55ca3808caeaac9
frazao-hub <angelofrazao34@yahoo.com>
2026-09-07 18:01:00 -0300

    Set up Firebase app initialization
    Initialize Firebase app with configuration settings.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,12 @@
-firebase.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8SnMWXagUVz4uVM87bc2dAakZyGGW3nM",
+  authDomain: "distribuidora-frazao.firebaseapp.com",
+  projectId: "distribuidora-frazao",
+  storageBucket: "distribuidora-frazao.firebasestorage.app",
+  messagingSenderId: "642540059204",
+  appId: "1:642540059204:web:c113fa6966fd97fc20da11"
+};
+
+export const app = initializeApp(firebaseConfig);

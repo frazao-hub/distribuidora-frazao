e489eed4f85ea9ed310ae3119847c4e103c5f85c
frazao-hub <angelofrazao34@yahoo.com>
2026-09-11 01:07:49 -0300

    Replace Firebase API key with placeholder
    Updated Firebase configuration with placeholder API key.

--- firebase.js
+++ firebase.js
@@ -65,7 +65,12 @@
 </div>
 
 <script>
-    // -------------------------------------------------------------
+    // ---apiKey: "AIzaSyD8SnMWXagUVz4uVM87bc2dAakZyGGW3nM",
+  authDomain: "distribuidora-frazao.firebaseapp.com",
+  projectId: "distribuidora-frazao",
+  storageBucket: "distribuidora-frazao.firebasestorage.app",
+  messagingSenderId: "642540059204",
+  appId: "1:642540059204:web:c113fa6966fd97fc20da11"----------------------------------------------------------
     // ATENÇÃO: Substitua os dados abaixo pelas chaves do SEU Firebase
     // -------------------------------------------------------------
     const firebaseConfig = {
